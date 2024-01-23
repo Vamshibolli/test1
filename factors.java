15f793edca7de1220e3c32304d24868a6ed89250
Vamshibolli <vamshi8904@gmail.com>
2024-01-23 19:09:40 +0530

    project added

---
+++ factors.java
@@ -0,0 +1,20 @@
+class loops
+{
+ public static void main(String args[])
+ {
+  int num=10,i=1,count=0;
+  while(i<=10)
+  {
+   if(num%i==0)
+   {
+    count++;
+    System.out.println(i);
+   }
+    i++;
+  }
+   System.out.println(count);
+ }
+}
+  
+   
+   
